23ec11cb59a8c518cccabcb184fc3395aff6d910
carolsol23 <s-carolina.solar-mor@lwtech.edu>
2026-02-07 21:40:41 -0800

    Add table to the database
    (cherry picked from commit 8ae4e92b6801c902b5634f6af42caada0f466664)

---
+++ Database-Group2-Carolina/Database.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE IF NOT EXISTS user_id;
+USE user_id;
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(30) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
+INSERT INTO users (username, password)
+VALUES ('admin', 'password123');
+-- Find a user by username
+SELECT *
+FROM users
+WHERE username = 'admin';
