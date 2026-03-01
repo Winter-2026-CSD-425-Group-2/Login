922d28283a8e202446d73291a5bb40c4373ccbba
Daniel McCord <daniel.mccord@lwtech.edu>
2026-03-01 04:21:23 -0800

    Prevent duplicate CORS configuration

--- backend/authentication.py
+++ backend/authentication.py
@@ -34,8 +34,7 @@ def build_response(status_code, body):
     return {
         "statusCode": status_code,
         "headers": {
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "*"
+            "Content-Type": "application/json"
         },
         "body": json.dumps(body)
     }
